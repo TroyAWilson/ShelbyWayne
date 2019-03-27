49f29b9436b2d00ece442cff89d9cf0b34817fa9
TroyAWilson <troyaustynwilson@gmail.com>
2019-03-27 13:07:57 -0700

    Implemented to top button
    Button that takes you to the top and back to the navigation.
    
    Changed a few things in the site text.
    
    Fixed the size of parralax images

--- scripts/script.js
+++ scripts/script.js
@@ -32,7 +32,7 @@ flower.onclick = function(){
     setTimeout(function(){menu.style.transform = "translateX(0px)"},100);
   }
   else{
-    menu.style.transform = 'translateX(500px)';
+    menu.style.transform = 'translateX(1000px)';
     setTimeout(function(){menu.style.display = "none"},300);
   };
 };
@@ -100,4 +100,21 @@ button = document.getElementsByClassName('contact-submit-button');
 button[0].onclick = function(){
   console.log("????????")
   button[0].preventDefault();
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
